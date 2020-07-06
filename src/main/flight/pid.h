266bdbd494aca4a8d68706abdd1b1f7c009e2d38
Petri Mattila <petri.t.j.mattila@gmail.com>
2020-07-06 16:44:07 +0100

    WIP: Helicopter support

--- src/main/flight/pid.h
+++ src/main/flight/pid.h
@@ -159,6 +159,22 @@ typedef struct pidProfile_s {
     uint8_t dyn_lpf_curve_expo;             // set the curve for dynamic dterm lowpass filter
     uint8_t level_race_mode;                // NFE race mode - when true pitch setpoint calcualtion is gyro based in level mode
     uint8_t vbat_sag_compensation;          // Reduce motor output by this percentage of the maximum compensation amount
+
+    // HF3D parameters
+    uint16_t yawColKf;                      // Feedforward for collective into Yaw
+    uint16_t yawColPulseKf;                 // Feedforward for collective impulse into Yaw
+    uint16_t yawCycKf;                      // Feedforward for cyclic into Yaw
+    uint16_t yawBaseThrust;                 // Base thrust for the tail
+    uint16_t rescue_collective;             // Collective pitch command when rescue is fully upright
+    uint8_t error_decay_always;             // Always decay accumulated I term and Abs Control error?
+    uint8_t error_decay_rate;               // Rate to decay accumulated error in deg/s
+    uint16_t collective_ff_impulse_freq;    // Collective input impulse high-pass filter cutoff frequency
+    
+    uint16_t elevator_filter_gain;          // Elevator stop de-bounce feedforward filter gain
+    uint8_t elevator_filter_window_time;    // Time in ms that we no longer apply de-bounce inside our window
+    uint8_t elevator_filter_window_size;    // Size of the de-bounce window around center stick (0 deg/s) in degrees/second
+    uint8_t elevator_filter_hz;             // Low-pass filter cutoff frequency that is applied to our elevator setpoint.  Lower Hz = more delay on stop.
+
 } pidProfile_t;
 
 PG_DECLARE_ARRAY(pidProfile_t, PID_PROFILE_COUNT, pidProfiles);
@@ -229,3 +245,8 @@ float pidGetFfBoostFactor();
 float pidGetFfSmoothFactor();
 float pidGetSpikeLimitInverse();
 float dynDtermLpfCutoffFreq(float throttle, uint16_t dynLpfMin, uint16_t dynLpfMax, uint8_t expo);
+float pidGetCollectiveStickPercent();
+float pidGetCollectiveStickHPF();
+uint16_t pidGetRescueCollectiveSetting();
+float pidGetCollectivePulseFilterGain();
+
