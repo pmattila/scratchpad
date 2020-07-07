1af43368f0696b1e17b25d95bb9ea44e818d6147
Petri Mattila <petri.t.j.mattila@gmail.com>
2020-07-07 10:21:17 +0100

    HF3D: rpm_filter reworked for Heliflight 3D
    This commit makes rpm_filter more generic for helicopter use.
    
    There are 16 filter banks, each filtering one frequency peak
    from roll, pitch, and yaw gyro signal, respectively.
    
    Each bank can be configured separately, with
      - RPM source from any motor
      - Custom Gear Ratio
      - Filter Q
      - Filter min and max Hz
    
    A typical use case would cover the following peaks:
      - main rotor fundamental
      - main rotor 2. harmonic
      - tail rotor fundamental
      - tail rotor 2. harmonic
      - motor fundamental
    
    On a more noisy helicopter, it would be possible to
    have dual notches on the strongest peaks:
      - main rotor fundamental x 0.99
      - main rotor fundamental x 1.01
      - main rotor 2. harmonic x 0.99
      - main rotor 2. harmonic x 1.01
      - main rotor 3. harmonic
      - main rotor 4. harmonic
      - tail rotor fundamental
      - tail rotor 2. harmonic
      - motor fundamental
    
    As always, blackbox log and frequency analysis of the remaining
    noise will give a good idea if the filter is performaing well.
    
    The following CLI variables have been added:
    
      gyro_rpm_filter_bank_motor_index
      gyro_rpm_filter_bank_gear_ratio
      gyro_rpm_filter_bank_notch_q
      gyro_rpm_filter_bank_min_hz
      gyro_rpm_filter_bank_max_hz
    
    motor_index:
      0 - bank disabled, 1 - first motor, 2 - second motor
    
    gear_ratio:
      Ratio between the RPM source (motor) and the peak frequency.
      x1000.
    
      For example, main rotor fundamental peak on Trex-450 with 121T
      main gear and 12T pinion would be 10083, and the second main
      rotor harmonic would be 5042.
    
    notch_q:
      Notch filter Q value x100.
    
    min_hz:
      Minimum allowed frequency for the notch.
    
    max_hz:
      Maximum allowed frequency for the notch.

--- src/main/flight/pid.c
+++ src/main/flight/pid.c
@@ -1055,15 +1055,13 @@ void FAST_CODE pidController(const pidProfile_t *pidProfile, timeUs_t currentTim
     float gyroRateDterm[XYZ_AXIS_COUNT];
     for (int axis = FD_ROLL; axis <= FD_YAW; ++axis) {
         gyroRateDterm[axis] = gyro.gyroADCf[axis];
-#ifdef USE_RPM_FILTER
-        gyroRateDterm[axis] = rpmFilterDterm(axis,gyroRateDterm[axis]);
-#endif
         gyroRateDterm[axis] = dtermNotchApplyFn((filter_t *) &dtermNotch[axis], gyroRateDterm[axis]);
         gyroRateDterm[axis] = dtermLowpassApplyFn((filter_t *) &dtermLowpass[axis], gyroRateDterm[axis]);
         gyroRateDterm[axis] = dtermLowpass2ApplyFn((filter_t *) &dtermLowpass2[axis], gyroRateDterm[axis]);
     }
 
     rotateItermAndAxisError();
+
 #ifdef USE_RPM_FILTER
     rpmFilterUpdate();
 #endif
@@ -1406,4 +1404,3 @@ float pidGetCollectivePulseFilterGain(void)
 {
     return collectivePulseFilterGain;
 }
-
