02409578ccde65752da18d9ffdb92aeed7f8a5de
Petri Mattila <petri.t.j.mattila@gmail.com>
2020-07-12 22:04:58 +0100

    HF3D: Remove BATTERY_VOLTAGE_SAG_COMPENSATION

--- src/main/config/config.c
+++ src/main/config/config.c
@@ -263,12 +263,6 @@ static void validateAndFixConfig(void)
                 pidProfilesMutable(i)->d_min[axis] = 0;
             }
         }
-
-#if defined(USE_BATTERY_VOLTAGE_SAG_COMPENSATION)
-        if (batteryConfig()->voltageMeterSource != VOLTAGE_METER_ADC) {
-            pidProfilesMutable(i)->vbat_sag_compensation = 0;
-        }
-#endif
     }
 
     if (motorConfig()->dev.motorPwmProtocol == PWM_TYPE_BRUSHED) {
