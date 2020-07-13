b624bb763deae5507f4b74c9cc41e440fd4c366e
Petri Mattila <petri.t.j.mattila@gmail.com>
2020-07-13 09:40:57 +0100

    HF3D: Remove ANTI_GRAVITY

--- src/main/fc/core.c
+++ src/main/fc/core.c
@@ -1008,8 +1008,6 @@ bool processRx(timeUs_t currentTimeUs)
     }
 #endif
 
-    pidSetAntiGravityState(IS_RC_MODE_ACTIVE(BOXANTIGRAVITY) || featureIsEnabled(FEATURE_ANTI_GRAVITY));
-
     return true;
 }
 
