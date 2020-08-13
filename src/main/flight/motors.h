7a24f05d9ef7c971c51fb97bae5156dc06051664
Petri Mattila <petri.t.j.mattila@gmail.com>
2020-08-14 00:39:00 +0100

    HF3D: Code refactoring in mixer/motors/servos.c
    This commit reworks most of the code in src/main/flight/
    
    All drone related code is removed, and a new generic mixer
    is implemented. Servo and motor handling is streamlined.

---
+++ src/main/flight/motors.h
@@ -0,0 +1,62 @@
+/*
+ * This file is part of Heliflight 3D.
+ *
+ * Heliflight 3D is free software. You can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Heliflight 3D is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this software. If not, see <https://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include "platform.h"
+
+
+typedef enum {
+    RPM_SRC_NONE = 0,
+    RPM_SRC_DSHOT_TELEM,
+    RPM_SRC_FREQ_SENSOR,
+    RPM_SRC_ESC_SENSOR,
+} rpmSource_e;
+
+
+extern uint8_t motorCount;
+
+extern float motorOutput[];
+extern float motorOutputMin;
+extern float motorOutputMax;
+extern float motorOutputStop;
+extern float motorOutputRange;
+
+
+void initRpmSource(void);
+void initEscEndpoints(void);
+
+void motorInit(void);
+void motorStop(void);
+void motorUpdate(void);
+
+bool areMotorsRunning(void);
+bool isRpmSourceActive(void);
+
+int getMotorRPM(uint8_t motor);
+int calcMotorRpm(uint8_t motor, int erpm);
+
+void motorResetDisarmed(void);
+void motorSetDisarmed(uint8_t motor, uint32_t value);
+
+
+// HF3D compat:
+#define stopMotors()      motorStop()
+#define getMotorCount()   motorCount
+#define motorOutputLow    motorOutputMin
+#define motorOutputHigh   motorOutputMax
+
