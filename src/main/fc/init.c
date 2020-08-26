31f1526c8a1eee7c8b3b3fadbd1bcd53615fdd78
Petri Mattila <petri.t.j.mattila@gmail.com>
2020-08-26 14:51:28 +0100

    HF3D: Remove motor and servo mixers
    This commit is preparation for a larger shuffle
    around mixers, motors, and servos.

--- src/main/fc/init.c
+++ src/main/fc/init.c
@@ -577,7 +577,7 @@ void init(void)
     serialInit(featureIsEnabled(FEATURE_SOFTSERIAL), SERIAL_PORT_NONE);
 #endif
 
-    mixerInit(mixerConfig()->mixerMode);
+    mixerInit();
     mixerConfigureOutput();
 
     uint16_t idlePulse = motorConfig()->mincommand;
@@ -852,11 +852,6 @@ void init(void)
     blackboxInit();
 #endif
 
-#ifdef USE_ACC
-    if (mixerConfig()->mixerMode == MIXER_GIMBAL) {
-        accStartCalibration();
-    }
-#endif
     gyroStartCalibration(false);
 #ifdef USE_BARO
     baroStartCalibration();
