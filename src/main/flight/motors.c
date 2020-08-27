938099658a7689c2a42ec8d6bc1aa16ee69c4ae5
James-T1 <3042407+James-T1@users.noreply.github.com>
2020-08-27 18:24:41 +0100

    HF3D: Add helicopter rotor speed governor

--- src/main/flight/motors.c
+++ src/main/flight/motors.c
@@ -238,8 +238,3 @@ void motorResetDisarmed(void)
         motorOutputDisarmed[i] = motorOutputStop;
 }
 
-float getHeadSpeed(void)
-{
-    return 0; // TODO
-}
-
--- src/main/flight/motors.h
+++ src/main/flight/motors.h
@@ -57,7 +57,5 @@ void motorUpdate(void);
 void motorResetDisarmed(void);
 void motorSetDisarmed(uint8_t motor, uint32_t value);
 
-float getHeadSpeed(void);
-
 // HF3D compat:
 #define stopMotors()  motorStop()
