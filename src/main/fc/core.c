heliflight3d/heliflight

HF3D: Free up space in ITCM RAM
Using FAST_CODE on a function will place that function
and anything the function is calling in ITCM RAM.

Before:
        ITCM_RAM:       15280 B        16 KB     93.26%

After:
        ITCM_RAM:       10568 B        16 KB     64.50%

src/main/fc/core.c
@@ -752,7 +752,7 @@ void subTaskTelemetryPollSensors(timeUs_t currentTimeUs)
 }
 #endif
 
-static FAST_CODE void subTaskMixerUpdate(timeUs_t currentTimeUs)
+static FAST_CODE_NOINLINE void subTaskMixerUpdate(timeUs_t currentTimeUs)
 {
     UNUSED(currentTimeUs);
 

pmattila/scratchpad

src/main/fc/core.c
@@ -1177,7 +1177,7 @@ void subTaskTelemetryPollSensors(timeUs_t currentTimeUs)
 }
 #endif
 
-static FAST_CODE void subTaskMotorUpdate(timeUs_t currentTimeUs)
+static FAST_CODE_NOINLINE void subTaskMotorUpdate(timeUs_t currentTimeUs)
 {
     uint32_t startTime = 0;
     if (debugMode == DEBUG_CYCLETIME) {